1f3d1b6cef47985f1397cb05f82cff7071ce15d9
abizmo <8437730+abizmo@users.noreply.github.com>
2025-05-11 22:18:26 +0100

    Add form control components and styles for improved form handling and validation

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,73 +1,71 @@
 import type { PropsWithChildren } from "preact/compat";
 import { useState } from "preact/hooks";
-import { z } from "zod";
 
+import { extractFieldErrors } from "../schemas/ticket-schema";
 import DropInput from "./drop-input";
+import FormControl from "./form-control";
 import FormLabel from "./form-label";
 import FormMessage from "./form-message";
+import styles from "./form.module.css";
+import Input from "./input";
 
-const schema = z.object({
-  file: z
-    .instanceof(File, { message: "A photo is required." })
-    .refine(
-      (file) => file.size <= 500 * 1024,
-      "File too large. Please upload a photo under 500KB.",
-    ),
-});
+import type { FieldErrors, Key, TicketSchema } from "../schemas/ticket-schema";
 
 export default function Form({ children }: PropsWithChildren) {
-  const [file, setFile] = useState<File | null>(null);
-  const [preview, setPreview] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
+  const [fields, setFields] = useState<TicketSchema>({});
+  const [errors, setErrors] = useState<FieldErrors>({});
 
   const handleFile = (file?: File) => {
-    setFile(file || null);
-    setError(null);
+    setFields((state) => ({ ...state, file: file }));
+    setErrors((errors) => ({ ...errors, file: undefined }));
   };
 
   const handleSubmit = (e: Event) => {
     e.preventDefault();
 
-    const result = schema.safeParse({ file });
+    const extractedErrors = extractFieldErrors(fields);
 
-    if (!result.success) {
-      setError(result.error.format().file?._errors[0] ?? "Error desconocido");
+    if (extractedErrors !== null) {
+      setErrors(extractedErrors);
       return;
     }
 
     // ✅ archivo válido
-    console.log("Archivo válido:", result.data.file);
+    console.log("Archivo válido:", fields.file);
 
     // Aquí podrías subir el archivo con fetch o FormData
   };
 
+  const handleInput = (key: Key, value: string) => {
+    setFields((state) => ({ ...state, [key]: value }));
+  };
+
+  const { file, fullName, email, ghUsername } = errors;
+
   return (
     <form onSubmit={handleSubmit}>
-      <FormLabel>Upload Avatar</FormLabel>
-      <DropInput onChange={handleFile} />
-      {/* <div
-        class="dropzone"
-        onDragOver={(e) => e.preventDefault()}
-        onDrop={handleDrop}
-        onClick={() => inputRef.current?.click()}
-      >
-        {preview ? (
-          <img src={preview} alt="preview" class="preview" />
-        ) : (
-          <p>Arrastra un archivo aquí o haz clic</p>
-        )}
-        <input
-          type="file"
-          id="avatar"
-          ref={inputRef}
-          class="hidden"
-          onChange={handleChange}
-        />
-      </div> */}
-      <FormMessage error={error !== null}>
-        {error ? error : "Upload your photo (JPG or PNG, max size: 500KB)."}
-      </FormMessage>
-
+      <FormControl>
+        <FormLabel>Upload Avatar</FormLabel>
+        <DropInput onChange={handleFile} />
+        <FormMessage error={file !== undefined}>
+          {file ? file : "Upload your photo (JPG or PNG, max size: 500KB)."}
+        </FormMessage>
+      </FormControl>
+      <FormControl className={styles.formControl}>
+        <FormLabel>Full Name</FormLabel>
+        <Input id="fullName" onInput={handleInput} error={!!fullName} />
+        {fullName && <FormMessage error>{fullName}</FormMessage>}
+      </FormControl>
+      <FormControl className={styles.formControl}>
+        <FormLabel>Email Address</FormLabel>
+        <Input id="email" onInput={handleInput} error={!!email} />
+        {email && <FormMessage error>{email}</FormMessage>}
+      </FormControl>
+      <FormControl className={styles.formControl}>
+        <FormLabel>Github Username</FormLabel>
+        <Input id="ghUsername" onInput={handleInput} error={!!ghUsername} />
+        {ghUsername && <FormMessage error>{ghUsername}</FormMessage>}
+      </FormControl>
       {children}
     </form>
   );
---
+++ src/schemas/ticket-schema.ts
@@ -0,0 +1,49 @@
+import { z } from "zod";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "A photo is required." })
+    .refine(
+      (file) => file.size <= 500 * 1024,
+      "File too large. Please upload a photo under 500KB.",
+    ),
+  fullName: z
+    .string({ required_error: "This field is required" })
+    .min(3, "Name must be 3 or more characters long."),
+  email: z
+    .string({ required_error: "This field is required" })
+    .email("Please enter a valid email address."),
+  ghUsername: z
+    .string({ required_error: "This field is required" })
+    .startsWith("@", "Must start with @")
+    .min(4, "Username must be 3 or more characters long."),
+});
+
+export type TicketSchema = Partial<z.infer<typeof schema>>;
+export type Key = keyof TicketSchema;
+export type FieldErrors = Partial<Record<keyof TicketSchema, string>>;
+
+export function extractFieldErrors(fields: TicketSchema) {
+  const result = schema.safeParse(fields);
+
+  if (result.success) return null;
+
+  const formatted = result.error.format();
+
+  const extractedErrors = Object.entries(formatted).reduce(
+    (acc, [key, value]) => {
+      if (key === "_errors") return acc; // Ignora errores generales
+      if (
+        typeof value === "object" &&
+        "_errors" in value &&
+        value._errors?.[0]
+      ) {
+        acc[key as keyof TicketSchema] = value._errors[0];
+      }
+      return acc;
+    },
+    {} as FieldErrors,
+  );
+
+  return extractedErrors;
+}
